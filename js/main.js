7b94925fa16b9c3b95a3cb7a51e8f2daa3bcb6b7
Artur <artur.prado@gmail.com>
2018-05-01 13:56:20 -0300

    Allow user to land on a specific profile

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function selectTab(profile) {
+  var elements = document.getElementsByClassName("intro-content");
+
+  if (profile === 'speaker') {
+    elements[1].click();
+  } else if (profile === 'developer') {
+    elements[0].click();
+  }
+}
+
+var profile = getParameterByName('profile');
+if (profile) {
+  selectTab(profile);
+}
